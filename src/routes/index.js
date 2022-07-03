a75574c6f02adf4e8da55e374e8d5e8ac4287b2d
Gongmeda <gongmeda@gmail.com>
2022-07-04 00:05:52 +0900

    refactor: eslint 설정에 맞게 코드 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: '안드로메다 폰' });
 });
 
-router.get('/pricing', function(req, res, next) {
+router.get('/pricing', function(req, res) {
   res.render('pricing', { title: '안드로메다 폰 가격' });
 });
 
-router.get('/checkout', function(req, res, next) {
+router.get('/checkout', function(req, res) {
   res.render('checkout', {title: '안드로메다 폰 구매', name: req.query.name, price: req.query.price});
 });
 
